70a5ce36469af1209869f04118513724db79da2e
chxgbx <yungryce@yahoo.com>
2024-05-27 15:59:41 +0000

    Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
+  const self = this
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
 
