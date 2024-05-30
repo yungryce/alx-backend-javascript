9bc54d2b73f688f1927453a100c77e75e2cde902
chxgbx <yungryce@yahoo.com>
2024-05-30 12:42:44 +0000

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -43,4 +43,4 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents;
+export default { listOfStudents };
