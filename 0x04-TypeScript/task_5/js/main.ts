eb1a3ac7531d9300da265f4378c07959046cb8f6
chxgbx <yungryce@yahoo.com>
2024-06-07 20:54:15 +0000

    Brand convention & Nominal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,46 +1,26 @@
-interface Student {
-  firstName: string,
-  lastName: string,
-  age: Number,
-  location: string
+interface  MajorCredits{
+credits: number;
 }
 
-const student1: Student = {
-  firstName: "Kalkidan",
-  lastName: "Demes",
-  age: 25,
-  location: "Ethiopia"
+interface  MinorCredits{
+credits: number;
 }
 
-const student2: Student = {
-  firstName: "Tester",
-  lastName: "unknown",
-  age: 30,
-  location: "Kenya"
+interface Major extends MajorCredits{
+  credits: number;
 }
 
-const studentsList: Array<Student> = [ student1, student2 ];
+interface Minor extends MinorCredits{
+  credits: number;
+}
 
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-const table: HTMLTableElement = document.createElement("table");
-const thead: HTMLTableSectionElement = document.createElement("thead");
-const tbody: HTMLTableSectionElement = document.createElement("tbody");
-const rowHead: HTMLTableRowElement = thead.insertRow(0);
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
-
-cell1Head.innerHTML = "firstName";
-cell2Head.innerHTML = "location";
-table.append(thead);
-
-studentsList.forEach((student) => {
-  const row: HTMLTableRowElement = tbody.insertRow(0);
-  const cell1: HTMLTableCellElement = row.insertCell(0);
-  const cell2: HTMLTableCellElement = row.insertCell(1);
-
-  cell1.innerHTML = student.firstName;
-  cell2.innerHTML = student.location;
-});
-
-table.append(tbody);
-body.append(table);
+function sumMajorCredits(subject1: Major, subject2: Major): Major {
+  return {
+    credits: subject1.credits + subject2.credits
+  }
+}
+ function sumMinorCredits(subject1: Minor, subject2: Minor): Minor {
+  return {
+    credits: subject1.credits + subject2.credits
+  }
+}
