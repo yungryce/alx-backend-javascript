998b1f3e2f4e464981334e89ebbc0f0c4c2708a1
chxgbx <yungryce@yahoo.com>
2024-07-22 20:53:34 +0100

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+
 function displayMessage(message) {
     console.log(message);
 }
